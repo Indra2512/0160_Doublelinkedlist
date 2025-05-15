2d7cbdc7b5cfe6fc4962da2f65f7907d923705b7
Indra2512 <mononeon1225@gmail.com>
2025-05-15 10:39:29 +0700

    step 4 lagi

--- DoubleLinkList.cpp
+++ DoubleLinkList.cpp
@@ -118,7 +118,12 @@ class DoubleLinkedList
                     //step 3: Link previous node to next of current
                     current->prev->next = current->next;
 
-                    
+                    //step 4: if current is not the last node
+                    if (current->next != NULL)
+                        current->next->prev = current->prev;
                 }
+
+                
             }
+
 };
